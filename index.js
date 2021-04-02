5efd657a2c1445ae19cd86d23b227a92e49f4315
Abu Naser Dipu <abunaserdipu0@gmail.com>
2021-04-02 16:01:37 +0600

    daily products server ready to launch

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const app = express();
+const MongoClient = require("mongodb").MongoClient;
+const ObjectID = require("mongodb").ObjectID;
+const cors = require("cors");
+require("dotenv").config();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8tihy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  console.log("database error", err);
+  const productsCollection = client.db("dailyProducts").collection("products");
+  const ordersCollection = client.db("dailyProducts").collection("orders");
+  console.log("database connected successfully");
+
+  app.get("/products", (req, res) => {
+    productsCollection.find().toArray((err, items) => {
+      res.send(items);
+    });
+  });
+
+  app.post("/addProduct", (req, res) => {
+    const newProduct = req.body;
+    console.log("adding new product", newProduct);
+    productsCollection.insertOne(newProduct).then((result) => {
+      console.log("inserted count", result.insertedCount);
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.get("/product/:id", (req, res) => {
+    const id = ObjectID(req.params.id);
+    productsCollection.find({ _id: id }).toArray((err, documents) => {
+      res.send(documents[0]);
+    });
+  });
+
+  app.post("/addOrder", (req, res) => {
+    const order = req.body;
+    console.log("adding new product", order);
+    ordersCollection.insertOne(order).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.get("/orders", (req, res) => {
+    ordersCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+
+  app.get("/manageProduct", (req, res) => {
+    productsCollection.find().toArray((err, items) => {
+      res.send(items);
+    });
+  });
+
+  app.delete("/deleteProduct/:id", (req, res) => {
+    productsCollection
+      .deleteOne({ _id: ObjectID(req.params.id) })
+      .then((result) => {
+        console.log(result);
+      });
+  });
+});
+
+app.listen(port);
